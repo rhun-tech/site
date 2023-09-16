e80414d72c7bcf79975327ed49e00aeed9ccf85f
rhun-tech <me@rhun.tech>
2023-09-16 10:06:41 -0400

    Toast cleanup.

--- js/futhark.js
+++ js/futhark.js
@@ -1,28 +1,29 @@
 function fetchAndCreateCard() {
-    var url = 'json/futhark.json';
-  
-    return fetch(url)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (data) {
-        const randomIndex = Math.floor(Math.random() * data.length);
-        const randomRune = data[randomIndex];
-  
-        const cardHTML = `
+  var url = 'json/futhark.json';
+
+  return fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      const randomIndex = Math.floor(Math.random() * data.length);
+      const randomRune = data[randomIndex];
+
+      const cardHTML = `
           <div class="card rhun-text">
             <div class="card-body">
-              <div class="card-title">Futhark</div>
+              <div class="card-title">${randomRune.name}</div>
               <span class="card-text col-auto">${randomRune.rune}</span>
-              <span class="card-text col-auto">${randomRune.name}</span>
+            </div>
+            <div class="card-footer">
+              <span>${randomRune.inspiration}</span>
             </div>
           </div>
         `;
-  
-        return cardHTML;
-      })
-      .catch(function (err) {
-        console.log('error: ' + err);
-      });
-  }
-  
+
+      return cardHTML;
+    })
+    .catch(function (err) {
+      console.log('error: ' + err);
+    });
+}
