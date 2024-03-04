0707c09d5630fc12f51811c762da08133b9e4c8c
rhun-tech <me@rhun.tech>
2024-03-03 21:27:44 -0500

    Updated rhunfall transition.

--- js/components/background-rhunic.js
+++ js/components/background-rhunic.js
@@ -14,66 +14,73 @@ function shuffleArray(array) {
   }
 }
 
-function createTextLinesFromArray(textArray, fontSize, randomColor) {
-  // Shuffle the textArray randomly
-  shuffleArray(textArray);
+function moveArrayRight(textGroup, previousWidth) {
+  textGroup.setAttribute("transform", `translate(${previousWidth}, 0)`);
+}
 
-  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  svg.setAttribute("width", "200");
-  svg.setAttribute("height", textArray.length * fontSize);
+function createTextLinesFromArray(textArray, fontSize, randomColor, previousWidth) {
+  shuffleArray(textArray);
 
   const textGroup = document.createElementNS("http://www.w3.org/2000/svg", "g");
 
   for (let i = 0; i < textArray.length; i++) {
     const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
-    text.setAttribute("x", "100");
+    text.setAttribute("x", "10");
     text.setAttribute("y", i * fontSize);
     text.setAttribute("text-anchor", "middle");
     text.setAttribute("font-size", fontSize);
     text.setAttribute("fill", randomColor);
-
-    // Set initial opacity
     text.setAttribute("opacity", getRandomNumber(0.3, 1));
-
     text.textContent = textArray[i];
-
     textGroup.appendChild(text);
   }
 
+  moveArrayRight(textGroup, previousWidth);
+
+  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttribute("width", "2000");
+  svg.setAttribute("height", "20" + textArray.length * fontSize);
+
   svg.appendChild(textGroup);
-
-  const randomX = getRandomNumber(-100, window.innerWidth - 100);
-  const randomY = getRandomNumber(0, window.innerHeight - textArray.length * fontSize);
-
   svg.style.position = "absolute";
-  svg.style.left = `${randomX}px`;
-  svg.style.top = `${randomY}px`;
-  svg.style.zIndex = -1;
+  svg.style.left = `0px`;
+  svg.style.top = `0px`;
+  svg.style.zIndex = 1;
 
-  document.body.appendChild(svg);
+  var rhunfallDiv = document.getElementById('rhunfall');
 
-  let opacityOffset = Math.random(); // Random offset to create a variation in the wave
+  if (rhunfallDiv) {
+    rhunfallDiv.appendChild(svg);
+  } else {
+    console.error('The specified div does not exist.');
+  }
 
-  // Set interval to update opacity with a wave effect every 100 milliseconds (adjust as needed)
+  let opacityOffset = Math.random();
+
   setInterval(() => {
     textGroup.childNodes.forEach((text, index) => {
-      // Update opacity dynamically in a wave pattern
       const waveOpacity = Math.sin(opacityOffset + (index / textArray.length) * Math.PI) * 0.35 + 0.65;
       text.setAttribute("opacity", waveOpacity);
     });
 
-    opacityOffset += 0.1; // Adjust the speed of the wave effect
+    opacityOffset += 0.1;
   }, 84);
+
+  return previousWidth + 20;
 }
 
 const textArray = ["ᚠ", "ᚢ", "ᚦ", "ᚨ", "ᚱ", "ᚲ", "ᚷ", "ᚹ", "ᚺ", "ᚾ", "ᛁ", "ᛃ", "ᛇ", "ᛈ", "ᛉ", "ᛊ", "ᛏ", "ᛒ", "ᛖ", "ᛗ", "ᛚ", "ᛜ", "ᛞ", "ᛟ"];
+const repeatedTextArray = textArray.concat(textArray);
 const fontSize = 20;
 const colorArray = ["var(--text-color)",
   "var(--text-color-secondary)",
   "var(--text-color-tertiary)",
   "var(--text-color-callout)"];
 
-for (let i = 0; i < 20; i++) {
+let previousWidth = 0;
+
+for (let i = 0; i < 100; i++) {
   const randomColor = getRandomColorFromArray(colorArray);
-  createTextLinesFromArray(textArray, fontSize, randomColor);
+  previousWidth = createTextLinesFromArray(repeatedTextArray, fontSize, randomColor, previousWidth);
 }
+
