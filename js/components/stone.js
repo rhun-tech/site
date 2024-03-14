9596d2a22fe1f84062bb1bf5158d94ba651531da
rhun-tech <me@rhun.tech>
2024-03-14 07:34:16 -0400

    css work

--- js/components/stone.js
+++ js/components/stone.js
@@ -6,16 +6,15 @@ class Stone extends HTMLElement {
   connectedCallback() {
       const titleText = this.getAttribute('title') || 'Stone';
       const titleIcon = this.getAttribute('icon') || '';
+      const stoneBorder = this.getAttribute('border') || '';
       const content = this.innerHTML;
 
       this.innerHTML = `
               <div class="rhun-stone">
-                <div class="rhun-center">
-                <span class="iconify rhun-2" data-icon="${titleIcon}"></span>
-                </div>              
-
-                <hr />
-                
+                <div class="rhun-center ${stoneBorder}">
+                  <span class="iconify rhun-2" data-icon="${titleIcon}"></span>
+                  <div class="rhun-center">${titleText}</div>
+                </div>
                 <div class="rhun-stone-body">
                     ${content}
                 </div>
